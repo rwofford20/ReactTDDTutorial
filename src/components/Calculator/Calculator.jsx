fc6bb0d7e784acf48e8b9e7e294b225a1f1566bc
Rachel E Wofford <rwofford20@my.whitworth.edu>
2020-03-03 15:29:43 -0800

    Initial commit

---
+++ src/components/Calculator/Calculator.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import Display from '../Display/Display';
+import Keypad from '../Keypad/Keypad';
+
+import './Calculator.css';
+
+
+class Calculator extends Component {
+    state = {
+        //value to be displayed in <Display />
+        displayValue: '0',
+        // values to be displayed in number <Keys />
+        numbers: [],
+        // values to be displayed in operator <Keys />
+        operators: [],
+        // operator selected for a math operation
+        selectedOperator: '',
+        // stored value to use for math operations
+        storedValue: '',
+    };
+
+    callOperator = () => {
+        console.log('call operation');
+    };
+
+    setOperator = () => {
+        console.log('set operation');
+    };
+
+    updateDisplay = () => {
+        console.log('update display');
+    };
+
+    render = () => {
+        // unpack the component state by using Object Destructuring
+        const {displayValue, numbers, operators} = this.state;
+
+        return (
+            <div className="calculator-container">
+                <Display displayValue={displayValue} />
+                <Keypad 
+                    callOperator={this.callOperator}
+                    numbers={numbers}
+                    operators={operators}
+                    setOperator={this.setOperator}
+                    updateDisplay={this.updateDisplay}
+                />
+            </div>
+        );
+    };
+}
+
+export default Calculator;
